61f71d6a3dc7b135abd3703e65e86e2ee045bf6b
Matt Payne <Payne@MattPayne.org>
2024-12-28 09:12:26 -0600

    added fork me ribbon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: `
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
+  <a class="github-fork-ribbon" href="https://github.com/payne/sqlite-demo" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
     <main>
       <router-outlet></router-outlet>
     </main>
